8c3ae12966d550cb9914f3e8cb3701b43bcaf38d
thudiemrena-commits <thudiemrena@gmail.com>
2026-08-18 18:28:07 +0700

    Rename window title for Rayfield UI

--- MEOMEOscript.lua
+++ MEOMEOscript.lua
@@ -2,7 +2,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "AIMDRIFT-CDT🚗",
+    Name = "CDT-meomeosystem-cardealershiptycoon",
     LoadingTitle = "MeoMeo StayLike System",
     LoadingSubtitle = "🇻🇳LHkhánhviệt",
     ConfigurationSaving = { Enabled = false, FolderName = nil, FileName = "MeoMeoConfig" },
